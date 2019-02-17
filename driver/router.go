e7d03e4e527593b487737a11932d4e4261434458
Taiki Hayashi <hayashitaiki@team-lab.com>
2019-02-17 20:46:49 +0900

    byebye! gin

--- driver/router.go
+++ driver/router.go
@@ -4,22 +4,14 @@ import (
 	"github.com/grandcolline/clean-arch-demo/adapter/controllers"
 	"github.com/grandcolline/clean-arch-demo/driver/mysql"
 
-	// "github.com/gin-gonic/gin"
 	"github.com/go-chi/chi"
 	"net/http"
 )
-
-// var Router *gin.Engine
 
 func Serve() {
 	logger := &Logger{}
 	conn := mysql.Connect()
 	userController := controllers.NewUserController(conn, logger)
-
-	// router := gin.Default()
-	// router.POST("/users", func(c *gin.Context) { userController.Create(c) })
-	// router.GET("/users", func(c *gin.Context) { userController.FindByName(c) })
-	// Router = router
 
 	r := chi.NewRouter()
 	r.Get("/users", func(w http.ResponseWriter, r *http.Request) {
